4e3a89221bb8dde1c8ec05f814ae72af1f4b87ed
Aditya Sharma <adisharma4857@gmail.com>
2024-01-08 20:22:43 +0530

    Added Day 4

---
+++ Day 4/sumOfUniqueNums.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int sumOfUnique(vector<int>& nums) {
+    int ans = 0;
+    int cnt[101]{};
+    for (int& x : nums) {
+        if (++cnt[x] == 1) {
+            ans += x;
+        } else if (cnt[x] == 2) {
+            ans -= x;
+        }
+    }
+    return ans;
+}

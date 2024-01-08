3ead2a9823f19624b16c69f1625358e835a3399a
Aditya Sharma <adisharma4857@gmail.com>
2024-01-08 20:26:30 +0530

    Added Day 5

---
+++ Day 5/missingNumber.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include<iostream>
+using namespace std;
+
+int missingNumber(vector<int>& nums) {
+
+    int n = nums.size();
+    
+    int expectedSum = n * (n + 1) / 2;
+    
+    int actualSum = 0;
+    for (int i = 0; i < n; i++) {
+        actualSum += nums[i];
+    }
+
+    return expectedSum - actualSum;
+}

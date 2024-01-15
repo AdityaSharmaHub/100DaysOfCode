af855539b8be49833d2a1625d3a2e6180c57ec07
Aditya Sharma <adisharma4857@gmail.com>
2024-01-15 20:00:50 +0530

    Added Day 14

---
+++ Day 14/splitArrayLargestSum.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int splitArray(int A[] ,int N, int M) {
+    // code here
+    int start=0, end=0, mid, ans;
+    
+    if(M>N)
+    return -1;
+    
+    for(int i=0; i<N; i++)
+    {
+        start = max(start, A[i]);
+        end += A[i];
+    }
+    
+    while(start <= end)
+    {
+        mid = start + (end - start) / 2;
+        int pages=0, count=1;
+        
+        for(int i=0; i<N; i++)
+        {
+            pages += A[i];
+            if(pages > mid)
+            {
+                count++;
+                pages = A[i];
+            }
+        }
+        
+        if(count <= M)
+        {
+            ans = mid;
+            end = mid - 1; 
+        }
+        else
+        start = mid + 1;
+    }
+    
+    
+    return ans;
+}
+
+int main()
+{
+    
+    return 0;
+}

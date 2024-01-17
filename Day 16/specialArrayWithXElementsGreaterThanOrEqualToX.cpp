e0bc850aaa2caba7b538a32a9d08a8450291dd34
Aditya Sharma <adisharma4857@gmail.com>
2024-01-17 22:16:46 +0530

    Added Day 17

---
+++ Day 16/specialArrayWithXElementsGreaterThanOrEqualToX.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int specialArray(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+
+        for (int x = 0; x <= nums.size(); x++) {
+            int count = nums.size() - (std::lower_bound(nums.begin(), nums.end(), x) - nums.begin());
+            if (count == x) {
+                return x;
+            }
+        }
+
+        return -1;
+    }
+};
---
+++ Day 17/twoSumPointer.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& numbers, int target) {
+    int start = 0, end = numbers.size() - 1;
+    vector<int>ans;
+    while(start < end)
+    {
+        if(numbers[start] + numbers[end] == target)
+        {
+            ans.push_back(start+1);
+            ans.push_back(end+1);
+            return ans;
+        }
+        else if(numbers[start] + numbers[end] < target)
+        start++;
+        else
+        end--;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    
+    return 0;
+}
+

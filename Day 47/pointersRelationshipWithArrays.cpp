f0f72026f989e5258a57876f639adbfb53406609
Aditya Sharma <adisharma4857@gmail.com>
2024-02-17 16:49:48 +0530

    Updated Day 47

--- Day 47/pointersRelationshipWithArrays.cpp
+++ Day 47/pointersRelationshipWithArrays.cpp
@@ -7,28 +7,53 @@ int main()
     int *ptr = arr;
 
     // Print the address of the first element or 0th index
-    cout<<arr<<endl;
-    cout<<arr+0<<endl;
-    cout<<&arr[0]<<endl;
-    cout<<ptr<<endl;
+    // cout<<arr<<endl;
+    // cout<<arr+0<<endl;
+    // cout<<&arr[0]<<endl;
+    // cout<<ptr<<endl;
 
     // Print the address of the second element or 1st index
-    cout<<arr+1<<endl;
-    cout<<&arr[1]<<endl;   
+    // cout<<arr+1<<endl;
+    // cout<<&arr[1]<<endl;   
 
     // Print the value of first element or 0th index
-    cout<<arr[0]<<endl;
-    cout<<*arr<<endl;
-    cout<<*(arr+0)<<endl;
-    cout<<*ptr<<endl;
+    // cout<<arr[0]<<endl;
+    // cout<<*arr<<endl;
+    // cout<<*(arr+0)<<endl;
+    // cout<<*ptr<<endl;
 
     // Print address of every element 
-    for(int i=0; i<5; i++)
-    cout<<arr+i<<endl;
+    // for(int i=0; i<5; i++)
+    // cout<<arr+i<<endl;
 
     // Print value of every element 
+    // for(int i=0; i<5; i++)
+    // cout<<*(arr+i)<<endl;
+
+    // Print all the values
     for(int i=0; i<5; i++)
-    cout<<*(arr+i)<<endl;
+    cout<<ptr[i]<<" ";
+
+    cout<<endl;
+
+    // Print all the adrress
+    for(int i=0; i<5; i++)
+    cout<<ptr+i<<endl;
+
+    // Arithmetic Operations (ptr++, ptr--)
+    // for(int i=0; i<5; i++)
+    // {
+    //     cout<<*ptr<<" ";
+    //     ptr++;
+    // }
+
+    // Add
+    ptr = ptr+3;
+    cout<<*ptr<<endl;
+
+    // Sub
+    ptr = ptr-2;
+    cout<<*ptr<<endl;
 
     return 0;
 }

91d2de6e0b6648723932e74e33cd6dabf42d4b95
Aditya Sharma <adisharma4857@gmail.com>
2024-04-02 22:16:46 +0530

    Added Day 92

---
+++ Day 92 - Cloned a Linked Lists/01_Clone_a_linked_list_with_next_and_random_pointer.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+// Clone a linked list with next and random pointer
+// Problem Link - https://www.geeksforgeeks.org/problems/clone-a-linked-list-with-next-and-random-pointer/1
+
+// Time Complexity - O(n2)
+// Space Complexity - O(1)
+
+struct Node {
+    int data;
+    Node *next;
+    Node *arb;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        arb = NULL;
+    }
+};
+
+// Function to find arbitrary pointer
+Node *find(Node *curr1, Node *curr2, Node *x)
+{
+    if(x == NULL) // base case
+    return NULL;
+    
+    while(curr1 != x)
+    {
+        curr1 = curr1->next;
+        curr2 = curr2->next;
+    }
+    
+    return curr2;
+}
+
+Node *copyList(Node *head)
+{
+    //Write your code here
+    Node *headCopy = new Node(0);
+    Node *tailCopy = headCopy;
+    Node *temp = head;
+    
+    // Clone created without arbitrary pointer
+    while(temp)
+    {
+        tailCopy->next = new Node(temp->data);
+        tailCopy = tailCopy->next;
+        temp = temp->next;
+    }
+    
+    tailCopy = headCopy;
+    headCopy = headCopy->next;
+    delete tailCopy;
+    
+    // Assign arbitrary pointer to the clone linked list
+    tailCopy = headCopy;
+    temp = head;
+    
+    while(temp)
+    {
+        tailCopy->arb = find(head, headCopy, temp->arb);
+        tailCopy = tailCopy->next;
+        temp = temp->next;
+    }
+    
+    return headCopy;
+    }
+
+int main()
+{
+    
+    return 0;
+}
